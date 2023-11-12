cd67ef0f161a7df7c3375adb2ebf04c17b46ac26
Jay Jagpal <jay@strangemother.com>
2023-11-12 01:29:34 +0000

    Cleanup docs and call offset
    + import primary.router and ingress.app
    + config func
    + start_server_task func
    + docs

--- src/run.py
+++ src/run.py
@@ -1,48 +1,85 @@
+"""Run the frontend ingress. FastAPI WebSocket and HTTP on uvicorn.
 
+    py run.py
+
+"""
 import asyncio
 import uvicorn
 from fastapi import FastAPI
 from pathlib import Path
 
-import config as conf
+import config as conf_module
+from ingress import app
+from primary import router
 
 HERE = Path(__file__).parent.as_posix()
 
-from ingress import app, router
 
 async def main():
     """
     python -m uvicorn ingress:app --host 127.0.0.1 --port 9004  --reload
     """
-    config = uvicorn.Config(app, # "ingress:app",
-            host=conf.HOST,
-            port=conf.PORT,
+    config = get_config(app) # "ingress:app"
+    task = await start_server_task(config)
+    # return await run_default_server(config)
+    ## Ensure to wait for the task.
+    await task
+
+
+def get_config(target, config=None):
+    """Given a target app, Create and return a uvicorn Config object.
+
+        get_config()
+
+    The real source: site-packages/uvicorn/config.py
+    """
+    _conf = config or {}
+    new_conf = dict(host=conf_module.HOST,
+            port=conf_module.PORT,
             log_level="info",
-            reload=conf.RELOAD,
+            reload=conf_module.RELOAD,
             reload_dirs=[HERE],
             use_colors=False,
-            )
+            ws_max_size=conf_module.WS_MAX_SIZE,
+        )
 
-    return await run_config(config)
-    # return await run_default_server(config)
+    new_conf.update(_conf)
+    c = uvicorn.Config(target, **new_conf)
+    return c
 
 
 async def run_default_server(config):
+    """Run a uvicorn server in the default manner, without the
+    task offset and port capture of `start_server_task`
+
+    Return None
+    """
     server = uvicorn.Server(config)
     await server.serve()
 
 
-async def run_config(config):
-    server = uvicorn.Server(config)
-    task = asyncio.create_task(server.serve())
+async def start_server_task(config):
+    """Create the server and install it as an async task
+    Wait for the server to _start_ then inspect for the
+    open socket addresses. Finally call upon the router to
+    set the new socket names.
 
+    Return the server task cooroutine
+    """
+    server = uvicorn.Server(config)
+    # await router.prepare_backpipe()
+    task = asyncio.create_task(server.serve())
+    ## To capture the used ports we wait for the
+    # server.servers[].started, then grab those.
+    # As the task is already running, this is non-blocking.
     names = await get_socket_names(server)
 
-    conf.ADDRESSES = names
-    # await app.set_socket_addresses(names)
-    await router.set_primary_sockets(names)
+    conf_module.ADDRESSES = names
 
-    await task
+    ## Tell the router its addresses (usually 1 pair)
+    await router.set_primary_sockets(names)
+    # await app.set_socket_addresses(names)
+    return task
 
 
 async def get_socket_names(server):
