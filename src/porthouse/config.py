c8add464644b5a7cfc34eacd873861c47f3e57fb
Jay Jagpal <jay@strangemother.com>
2023-11-17 05:17:41 +0000

    App level config options

--- src/porthouse/config.py
+++ src/porthouse/config.py
@@ -1,12 +1,20 @@
+LOG_LEVEL = 'debug'
+
 
 HOST = '127.0.0.1'
 DEBUG = True
 RELOAD = DEBUG
 PORT = 0# 9004
 
-BALANCE_ADDRESSES = (HOST, (9004, 9005,), )
+BALANCE_PORTS = (9004, 9005,)
+BALANCE_ADDRESSES = (HOST, BALANCE_PORTS, )
+
+INGRESS_APP = 'porthouse.ingress:app'
+
+
 
 mb1 = 1024 * 1024
 mb16 = 16 * mb1
 
 WS_MAX_SIZE = mb1
+
