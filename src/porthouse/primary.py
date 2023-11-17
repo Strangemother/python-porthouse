6c84f21127169e24a429b939a56ed56a297a4972
Jay Jagpal <jay@strangemother.com>
2023-11-17 05:16:46 +0000

    Moved handler functions

--- src/porthouse/primary.py
+++ src/porthouse/primary.py
@@ -39,16 +39,6 @@ router = Router()
 adapter = adapters.get_adapter('starlette', router)
 
 
-def generate_typemap():
-    """Return a dict to map actions to methods. The key should be data['type']
-    If the data[type] is not mapped within this dict
-    """
-    return {
-        'websocket.disconnect': websocket_disconnect,
-        'default': default_action,
-    }
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     dlog('lifespan Startup')
@@ -65,55 +55,16 @@ async def primary_ingress(websocket, **kw):
     The router _accepts_ the socket then proceeds to wait for incoming mesages
     Upon a new message, call to the handle_message function
     """
-    websocket._ok = await router.websocket_accept(websocket, **kw)
+    websocket._ok = await adapter.websocket_accept(websocket, **kw)
+    # websocket._ok = await router.websocket_accept(websocket, **kw)
 
     while websocket._ok:
         # data = await router.receive()
         data = await adapter.wait_receive(websocket)
-        ok = await handle_message(websocket, data)
+        ok = await adapter.handle_message(websocket, data)
+        # ok = await handle_message(websocket, data)
     else:
-        if websocket.client_state.value == 1:  # websocket.CONNECTED
-            # await websocket.close()
-            await adapter.close(websocket)
-
-
-async def handle_message(websocket, data):
-    """Given a socket and the new message, read the `type` of message
-    and call the `typemap` handler function. If no function is found use
-    the `default_action` function.
-    If the result from the action method is not a truthy, the socket will
-    disconnect.
-
-    Return an `ok` truthy. `0` for _not ok_, `1` for okay.
-    """
-    action_func = typemap.get(data['type'], None) or typemap['default']
-    ok = await action_func(websocket, data)
-    return ok
-
-
-async def default_action(websocket, data):
-    """The default function for an incoming message from a websocket.
-    Call to the router recv_socket_event and return a _receipt_ back to the
-    client (if given).
-
-    Return `1` or any truthy as an "ok" to _continue listening_.
-    """
-    receipt = await router.recv_socket_event(websocket, data)
-    # 'send', 'send_bytes', 'send_json', 'send_text',
-    if receipt is not None:
-        await websocket.send_text(receipt)
-    return 1
-
-
-async def websocket_disconnect(websocket, data):
-    """Handle the "websocket.disconnect" event from the primary ingress.
-    Call upon the router websocket_disconnect method and flag the websocket as
-    not _ok_.
-    """
-    websocket._ok = 0
-    await router.websocket_disconnect(websocket, data)
-    # await live_register.remove(websocket)
-    return websocket._ok
-
-
-typemap = generate_typemap()
+        await adapter.wait_exit(websocket)
+        # if websocket.client_state.value == 1:  # websocket.CONNECTED
+        #     # await websocket.close()
+        #     await adapter.close(websocket)
