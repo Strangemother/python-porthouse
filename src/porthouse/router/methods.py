4dfa8a6a6fd0816a61524a9e5332e48fc06971dd
Jay Jagpal <jay@strangemother.com>
2023-12-08 01:49:46 +0000

    refactor the `Methods` to use a _generate_enum_ class.

--- src/porthouse/router/methods.py
+++ src/porthouse/router/methods.py
@@ -1,15 +1,27 @@
 from enum import Enum, auto
+from ..denum import generate_enum
+from ..dispatch.register import get_register
 
-class Methods(Enum):
-    SUPERCAST = 'supercast'
-    ROOMCAST =  'roomcast'
+
+"""Import members from the dispatch register and convert to an Enum class
+
+    class Methods(Enum):
+        SUPERCAST = 'supercast'
+        ROOMCAST =  'roomcast'
+"""
+members = {x.upper():x.lower() for x in get_register()}
+Methods = generate_enum('Methods', members)
 
 
 def keys():
+    """Return a list of enum member instances, available within the `Methods` enum class.
+    """
     return tuple(Methods)
 
 
 def values():
+    """Return the `.value` of each enum member from the `keys()` function.
+    """
     ks = keys()
     return tuple(x.value for x in ks)
 
